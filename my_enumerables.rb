f324edd8266f73367c93332a84ee6b90887e348a
prius2055 <prius2055@gmail.com>
2023-06-15 09:59:04 +0100

    fix linters

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,20 +1,19 @@
 module MyEnumerables
+  def all
+    each { |element| return false unless yield(element) }
+    true
+  end
 
-def all
-         each { |element| return false unless yield(element) }
-         true
-    end
+  def any
+    each { |element| return true if yield(element) }
+    false
+  end
 
-    def any
-        each { |element| return true if yield(element) }
-        false
+  def filter
+    result = []
+    self.each do |element|
+      result << element if yield(element)
     end
-
-    def filter
-         result = []
-         self.each do |element| 
-            result << element if yield(element)
-         end 
-         result.inspect
-    end
+    result.inspect
+  end
 end
