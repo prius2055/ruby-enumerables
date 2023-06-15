fd1f5d47eae2b66b6854995b41ed46e4c23b0b11
Damilare <bestbynature2010@gmail.com>
2023-06-15 09:45:40 +0100

    :rocket: Add Enumerable module

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,4 +1,20 @@
 module MyEnumerables
 
-# ADD THE CODE HERE
+def all
+         each { |element| return false unless yield(element) }
+         true
+    end
+
+    def any
+        each { |element| return true if yield(element) }
+        false
+    end
+
+    def filter
+         result = []
+         self.each do |element| 
+            result << element if yield(element)
+         end 
+         result.inspect
+    end
 end
